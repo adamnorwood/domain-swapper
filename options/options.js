ec5979dad209aadd69a00d6b10073ab4bbbc159b
Adam Norwood <adam@adamnorwood.com>
2020-06-20 15:44:46 -0500

    Revise markup and add styling for Option panel

--- options/options.js
+++ options/options.js
@@ -1,8 +1,9 @@
 const hostsContainer = document.getElementById('hosts');
 
 const addButton = document.createElement('button');
-addButton.innerHTML = 'Add host';
+addButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="100" height="100"><path d="M100 36.42H63.58V0H36.42v36.42H0v26.54h36.42V100h27.16V62.96H100z"/></svg> Add';
 addButton.type = 'button';
+addButton.classList.add('button--add');
 addButton.addEventListener('click', (e) => {
   e.preventDefault();
   hostsContainer.appendChild(hostInput());
@@ -16,7 +17,11 @@ function saveOptions(e) {
   let hosts = [];
   const inputs = document.querySelectorAll('input');
   for (const input of inputs) {
-    hosts.push(input.value)
+    if ('' !== input.value) {
+        hosts.push(input.value)
+    } else {
+      input.closest('div').remove();
+    }
   }
 
   browser.storage.local.set({
@@ -44,12 +49,14 @@ function hostInput(value) {
   const inputContainer = document.createElement('div');
   const input = document.createElement('input');
   input.value = value || '';
+  input.type = 'text';
 
   const removeButton = document.createElement('button');
   removeButton.type = 'button';
-  removeButton.innerText = 'Remove';
+  removeButton.classList.add('button--remove');
+  removeButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="100" height="100"><title>Remove</title><path d="M78.64 0L50 28.64 21.36 0 0 21.36 28.64 50 0 78.64l20.87 20.87 28.64-28.64L78.64 100 100 78.64 70.87 49.51l28.64-28.64z"/></svg>';
   removeButton.addEventListener('click', (e) => {
-    e.target.parentNode.remove();
+    e.target.closest('div').remove();
   });
 
   inputContainer.appendChild(input);
