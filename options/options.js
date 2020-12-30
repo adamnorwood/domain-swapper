8c76f7b8f4bff7ffec6a995005185a70f2b200a5
Adam Norwood <adam@adamnorwood.com>
2020-12-30 13:58:17 -0600

    Add styling and Saved status interaction for options panel
    Closes #2
    Closes #3

--- options/options.js
+++ options/options.js
@@ -1,13 +1,19 @@
 const hostsContainer = document.getElementById('hosts');
+const saveButton     = document.querySelector('.button--save');
+
 const addButton = document.createElement('button');
-addButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="100" height="100"><path d="M100 36.42H63.58V0H36.42v36.42H0v26.54h36.42V100h27.16V62.96H100z"/></svg> Add';
+addButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="100" height="100"><path d="M100 36.42H63.58V0H36.42v36.42H0v26.54h36.42V100h27.16V62.96H100z"/></svg> Add Domain';
 addButton.type = 'button';
 addButton.classList.add('button--add');
 addButton.addEventListener('click', (e) => {
   e.preventDefault();
   hostsContainer.appendChild(hostInput());
+  hostsContainer.dataset.count = hostsContainer.querySelectorAll('input').length;
+
+  saveButton.textContent = 'Save';
+  saveButton.removeAttribute('disabled');
 });
-hostsContainer.appendChild(addButton);
+document.querySelector('header').appendChild(addButton);
 
 function saveOptions(e) {
 
@@ -26,17 +32,24 @@ function saveOptions(e) {
   browser.storage.local.set({
     hosts: hosts
   });
+
+  hostsContainer.dataset.count = hostsContainer.querySelectorAll('input').length;
+
+  saveButton.textContent = 'Saved';
+  saveButton.setAttribute('disabled', 'disabled');
 }
 
 function restoreOptions() {
 
   function setCurrentChoice(result) {
-    if (result.hosts.length) {
+    if (result.hosts && result.hosts.length) {
       result.hosts.forEach(host => {
         hostsContainer.appendChild(hostInput(host));
       });
 
       hostsContainer.dataset.count = result.hosts.length;
+    } else {
+      hostsContainer.dataset.count = 0;
     }
   }
 
@@ -52,10 +65,18 @@ function hostInput(value) {
   const input = document.createElement('input');
   input.value = value || '';
   input.type = 'text';
+  input.setAttribute('placeholder', 'example.test');
 
   // Remove http:// and https:// from the input string.
   input.addEventListener('blur', (e) => {
     e.target.value = e.target.value.replace(/https?:\/\//gi, '');
+  });
+
+  input.addEventListener('keyup', (e) => {
+    if ('Enter' !== e.key) {
+      saveButton.textContent = 'Save';
+      saveButton.removeAttribute('disabled');
+    }
   });
 
   const removeButton = document.createElement('button');
@@ -64,6 +85,9 @@ function hostInput(value) {
   removeButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="100" height="100"><title>Remove</title><path d="M78.64 0L50 28.64 21.36 0 0 21.36 28.64 50 0 78.64l20.87 20.87 28.64-28.64L78.64 100 100 78.64 70.87 49.51l28.64-28.64z"/></svg>';
   removeButton.addEventListener('click', (e) => {
     e.target.closest('div').remove();
+    hostsContainer.dataset.count = hostsContainer.querySelectorAll('input').length;
+    saveButton.textContent = 'Save';
+    saveButton.removeAttribute('disabled');
   });
 
   inputContainer.appendChild(input);
