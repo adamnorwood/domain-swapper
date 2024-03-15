4ca98bbe64b6903c56eca88de6cddcd1a9cb2d20
Adam Norwood <adam@adamnorwood.com>
2024-03-15 18:50:04 -0500

    Formats domainsPopup with Prettier

--- components/domainsPopup.tsx
+++ components/domainsPopup.tsx
@@ -1,37 +1,60 @@
+import { useState } from "react"
+
 import { useAppDispatch, useAppSelector } from "~store"
 
 export const DomainsPopup = () => {
   const dispatch = useAppDispatch()
 
-  // Make sure to use "useAppSelector" instead of "useSelector" to automatically get the correct types.
+  // Make sure to use "useAppSelector" instead of "useSelector"
+  // to automatically get the correct types.
   const domains = useAppSelector((state) => state.domains)
 
-  return (
+  // For this popup of possible destinations, we don't want to show a
+  // domain if it's the one the user is currently already looking at.
+  // But since chrome.tabs.query returns a Promise, we'll need to use
+  // state to keep track of the current tab's host value.
+  const [currentHost, setcurrentHost] = useState({ host: null, protocol: null })
+
+  // As mentioned above, here's that Promise…
+  chrome.tabs.query({ active: true, currentWindow: true }).then((tab) => {
+    const currentURL = new URL(tab[0].url)
+    setcurrentHost({ host: currentURL.host, protocol: currentURL.protocol })
+  })
+
+  return ["https:", "http:"].includes(currentHost.protocol) ? (
     <ul>
-      {domains.domainsList.map((domain) => (
-        <li
-          key={domain.id}
-          onClick={(e) => {
-            chrome.tabs
-              .query({ active: true, currentWindow: true })
-              .then((tab) => {
-                const url = new URL(tab[0].url)
-                const newURL = new URL(`https://${domain.domain}/`)
-                url.host = newURL.host
-                url.hostname = newURL.hostname
-                url.port = newURL.port
+      {domains.domainsList.map(
+        (domain) =>
+          currentHost.host !== domain.domain && (
+            <li
+              key={domain.id}
+              onClick={(e) => {
+                chrome.tabs
+                  .query({ active: true, currentWindow: true })
+                  .then((tab) => {
+                    const url = new URL(tab[0].url)
+                    const newURL = new URL(`https://${domain.domain}/`)
+                    url.host = newURL.host
+                    url.hostname = newURL.hostname
+                    url.port = newURL.port
 
-                chrome.tabs.update(undefined, {
-                  url: url.href
-                })
+                    chrome.tabs.update(undefined, {
+                      url: url.href
+                    })
 
-                // Close the popup now that the user has selected a domain.
-                window.close()
-              })
-          }}>
-          {domain.domain}
-        </li>
-      ))}
+                    // Close the popup now that the user has selected a domain.
+                    window.close()
+                  })
+              }}>
+              {domain.domain}
+            </li>
+          )
+      )}
     </ul>
+  ) : (
+    <div className="wrong-protocol">
+      Sorry, you can only swap when you're on a normal (HTTP/HTTPS) page! (you
+      are currently on a <code>{currentHost.protocol}</code> page)
+    </div>
   )
 }
